7e01bdfa9263aa0af9a45deffe5befc90fe8b8dd
radiantly <buildingsomethingfun@gmail.com>
2024-04-28 21:41:23 +0530

    Add hero content

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,9 +1,35 @@
+import { GoArrowRight } from "react-icons/go";
+import { BsChevronDoubleDown } from "react-icons/bs";
 import NavBar from "./components/NavBar";
-
+import styles from "./css/App.module.css";
 function App() {
   return (
-    <div>
+    <div className={styles.hero}>
       <NavBar />
+      <div className={styles.thriller}>
+        <div className={styles.content}>
+          <div className={styles.desc}>
+            AI-powered server
+            <br />
+            monitoring and
+            <br />
+            management
+          </div>
+          <div className={styles.tagline}>
+            Health checks. Server hardening.
+            <br />
+            Resource usage alerts. Never been easier.
+          </div>
+          <div className={styles.demo}>
+            <div>See a demo</div>
+            <GoArrowRight className={styles.icon} />
+          </div>
+        </div>
+      </div>
+      <div className={styles.scrollWrap}>
+        <div>Scroll to learn more</div>
+        <BsChevronDoubleDown className={styles.icon} />
+      </div>
     </div>
   );
 }
